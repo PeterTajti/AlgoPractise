09766f97f522a0a9c8671ae5e8d9f10ebe86b08b
PeterTajti <peter.tajti@gmail.com>
2023-03-25 10:01:48 +0100

    Initial isConsecutive2: subtract two adjacent elements

--- src/IsConsecutive.java
+++ src/IsConsecutive.java
@@ -4,11 +4,12 @@ public class IsConsecutive {
     public static void main(String[] args) {
 
         System.out.println(isConsecutive(new int[]{3, 5, 2, 1, 4})); // true
-        System.out.println(isConsecutive(new int[]{3, 5, 2, 1, 6})); // false
+        System.out.println(isConsecutive2(new int[]{3, 5, 2, 1, 6})); // false
         System.out.println(isConsecutive(new int[]{7, 8, 6, 10, 11})); // false
-        System.out.println(isConsecutive(new int[]{4, 2, 3, 6, 5})); // true
+        System.out.println(isConsecutive2(new int[]{4, 2, 3, 6, 5})); // true
 
     }
+
     private static boolean isConsecutive(int[] input) {
 
         Arrays.sort(input);
@@ -16,11 +17,24 @@ public class IsConsecutive {
         int firstNumber = input[0];
 
         for (int i = 0; i < input.length; i++) {
-            if (input[i] != firstNumber){
+            if (input[i] != firstNumber) {
                 return false;
             }
             firstNumber++;
         }
+        return true;
+    }
+
+    private static boolean isConsecutive2(int[] input) {
+
+        Arrays.sort(input);
+
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i + 1] - input[i] != 1) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
