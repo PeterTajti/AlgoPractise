5931125f260e92472a51cc78521650ffe1922fc7
PeterTajti <peter.tajti@gmail.com>
2023-04-02 20:32:50 +0200

    Initial subtractionThreeFive.

---
+++ src/CodingChallenge/SubtractionThreeFive.java
@@ -0,0 +1,14 @@
+package CodingChallenge;
+
+public class SubtractionThreeFive {
+
+    public static void main(String[] args) {
+
+        System.out.println(subtractionThreeFive(3, 5)); // -2
+    }
+
+    private static int subtractionThreeFive(int a, int b){
+
+        return a-b;
+    }
+}
