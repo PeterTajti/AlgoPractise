4749d9962f91111922fa0a90c7d92fbd73c440b4
PeterTajti <peter.tajti@gmail.com>
2023-04-02 20:27:19 +0200

    Initial MultiplicationFiveThree.

---
+++ src/CodingChallenge/MultiplicationFiveThree.java
@@ -0,0 +1,14 @@
+package CodingChallenge;
+
+public class MultiplicationFiveThree {
+
+    public static void main(String[] args) {
+
+        System.out.println(multiplicationFiveThree(5, 3)); // 15
+    }
+
+    private static int multiplicationFiveThree(int a, int b){
+
+        return a*b;
+    }
+}
