693edc91cea9379a85057f4d22a51955bacd8e99
PeterTajti <peter.tajti@gmail.com>
2023-04-02 21:17:38 +0200

    Initial TrueOrFalse and print out PI.

---
+++ src/CodingChallenge/TrueOrFalse.java
@@ -0,0 +1,18 @@
+package CodingChallenge;
+
+public class TrueOrFalse {
+    public static void main(String[] args) {
+
+        System.out.println(5 > 3);
+        System.out.println(3 > 3);
+        System.out.println(3 >= 3);
+        System.out.println(1 < 2);
+        System.out.println(8 <= 4);
+        System.out.println(0 != 15);
+        System.out.println(9 == 9);
+        System.out.println(5 == 7);
+
+        System.out.println("Value of the pi approximately: " + Math.PI);
+        System.out.printf("Value of the pi approximately: %.2f", Math.PI);
+    }
+}
