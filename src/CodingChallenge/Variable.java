1ea00b631725918bc95a861cd8caaa0df70335d2
PeterTajti <peter.tajti@gmail.com>
2023-04-05 16:39:54 +0200

    Practise Variables.

---
+++ src/CodingChallenge/Variable.java
@@ -0,0 +1,26 @@
+package CodingChallenge;
+
+public class Variable {
+
+    public static void main(String[] args) {
+
+        int mouth = 4;
+        int day = 11;
+
+        int sumMouthAndDay = mouth + day;
+
+        byte sumFromOneToFive = 1 + 2 + 3 + 4 + 5;
+
+        //int shouldDouble = 5.5; Semantic error - int cannot store NOT whole number
+
+        double first;
+        int second = 2;
+
+        first = 2 * second;
+
+        System.out.println(first);
+
+        //second = first * 3.14; Semantic error - int cannot store NOT whole number
+
+    }
+}
