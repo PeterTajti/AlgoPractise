e5705547c3335001a0afba59cfdf268d008249b1
PeterTajti <peter.tajti@gmail.com>
2023-04-07 09:40:01 +0200

    Initial EnoughIsEnough task.

---
+++ src/Codewars/EnoughIsEnough.java
@@ -0,0 +1,21 @@
+package Codewars;
+
+import java.util.Arrays;
+
+// Given a list and a number, create a new list
+// that contains each number of list at most N times, without reordering.
+public class EnoughIsEnough {
+
+    public static void main(String[] args) {
+
+        int[] elements = {1, 2, 3, 1, 2, 1, 2, 3};
+
+        System.out.println(Arrays.toString(deleteNth(elements, 2))); // [1, 2, 3, 1, 2, 3]
+    }
+
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+
+
+        return null;
+    }
+}
