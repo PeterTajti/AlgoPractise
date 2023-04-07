f2c6207e95cd4dff731616c275911590ebca7017
PeterTajti <peter.tajti@gmail.com>
2023-04-07 18:01:52 +0200

    Add isConsecutive3 function.

--- src/IsConsecutive.java
+++ src/IsConsecutive.java
@@ -8,6 +8,13 @@ public class IsConsecutive {
         System.out.println(isConsecutive(new int[]{7, 8, 6, 10, 11})); // false
         System.out.println(isConsecutive2(new int[]{4, 2, 3, 6, 5})); // true
 
+        System.out.println();
+
+        System.out.println(isConsecutive3(new int[]{3, 5, 2, 1, 4})); // true
+        System.out.println(isConsecutive3(new int[]{3, 5, 2, 1, 6})); // false
+        System.out.println(isConsecutive3(new int[]{7, 8, 6, 10, 11})); // false
+        System.out.println(isConsecutive3(new int[]{4, 2, 3, 6, 5})); // true
+
     }
 
     private static boolean isConsecutive(int[] input) {
@@ -35,6 +42,18 @@ public class IsConsecutive {
             }
         }
 
+        return true;
+    }
+
+    private static boolean isConsecutive3(int[] input){
+
+        Arrays.sort(input);
+
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i] + 1 != input[i + 1]){
+                return false;
+            }
+        }
         return true;
     }
 }
