ecf3307495a806f3612c166a52dd73cef5433b89
PeterTajti <peter.tajti@gmail.com>
2023-04-07 18:38:03 +0200

    Initial averageOfEveryNth function.

---
+++ src/AverageOfEveryNth.java
@@ -0,0 +1,32 @@
+public class AverageOfEveryNth {
+
+    public static void main(String[] args) {
+
+        System.out.println(averageOfEveryNth(new int[]{2, 4, 10, 34, 3, 16, 3, 21}, 3)); // 13
+        System.out.println(averageOfEveryNth(new int[]{2, 4, 10, 34, 3, 16, 3, 21}, 2)); // 18.75
+        System.out.println(averageOfEveryNth(new int[]{2, 4, 10, 34, 3, 16, 3, 21}, 10)); // 0
+    }
+
+    private static double averageOfEveryNth(int[] numbers, int n) {
+
+        if (n > numbers.length) {
+            return 0;
+        }
+
+        int sum = 0;
+        int counter = 0;
+        int repeatNum = numbers.length / n;
+        int position = n;
+
+        for (int i = 0; i < repeatNum; i++) {
+
+            sum += numbers[position - 1];
+            position += n;
+            counter++;
+        }
+
+        double result = (double) sum / counter;
+
+        return result;
+    }
+}
