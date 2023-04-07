1a79f8225f30bfc9f5dfe57bc13d33a5a512cd2c
PeterTajti <peter.tajti@gmail.com>
2023-04-07 09:52:35 +0200

    Task details in comment.

--- src/Codewars/EnoughIsEnough.java
+++ src/Codewars/EnoughIsEnough.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 // Given a list and a number, create a new list
 // that contains each number of list at most N times, without reordering.
+// Delete occurrences of an element if it occurs more than n times
 public class EnoughIsEnough {
 
     public static void main(String[] args) {
