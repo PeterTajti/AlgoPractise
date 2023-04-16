7155c84097d771eaaafed51a88bab18bb4ad5fec
PeterTajti <peter.tajti@gmail.com>
2023-04-16 09:45:28 +0200

    Initial letterCounter.

---
+++ src/CodingChallenge/LetterCounter.java
@@ -0,0 +1,24 @@
+package CodingChallenge;
+
+public class LetterCounter {
+
+    public static void main(String[] args) {
+
+        String str = "Welcome to JavaTpoint!";
+        System.out.println(letterCounter(str, 'o'));
+    }
+    public static int letterCounter (String str, char c){
+
+        int counter = 0;
+        int index = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.indexOf(c, index) > -1){
+                counter++;
+                index = str.indexOf(c, index) + 1;
+            }
+        }
+        return counter;
+    }
+}
